409542b365a09b405f74d8a5f09b96dc49e8c43a
Umayrkaleem <Umayrkaleem@gmail.com>
2020-07-09 12:56:33 -0500

    adding to the who we are section on the home page

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -7,20 +7,16 @@ export class Home extends Component {
     return (
       <div>
         <h1>Welcome to U.H. Logistics</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+
+        <div class="row">
+        <div class="col s12">
+            <h5 class="center">Who We Are</h5>
+            <p class="light center"><span class='red-text'>U.H.</span> Logistics LLC is proudly based out of Texas, USA. We are a freight transportation company specializing in LTL freight. Our strongest focus is on our clients and being able to build a strong relationship with them; not to just service one of their transportation needs. We aim to continue to serve them in the highest standard in terms of service, dependability, communication, and overall client satisfaction. We truly understand the value of your cargo, how hard your team works, and how vital it is that every part of your company runs smoothly.</p>
+        </div>
       </div>
+      </div>
+
+
     );
   }
 }
